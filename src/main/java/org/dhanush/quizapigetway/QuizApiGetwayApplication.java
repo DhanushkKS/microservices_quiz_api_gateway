7fddf9eac4c7553652978f9e0dbd0159ac0bf234
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-02-21 21:07:59 +0530

    created api gateway

---
+++ src/main/java/org/dhanush/quizapigetway/QuizApiGetwayApplication.java
@@ -0,0 +1,13 @@
+package org.dhanush.quizapigetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApiGetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizApiGetwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dhanush/quizapigetway/QuizApiGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dhanush.quizapigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApiGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
